e844c5f5845137684858adab4a536ac350a95a31
Nahoor07 <nahoor98@gmail.com>
2023-07-10 12:58:37 +0500

    done edit profile

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,9 @@ Route::get('/detail', function () {
 Route::get('/all', function () {
     return Inertia::render('AllDetail');
 })->name('all.detail');
+Route::get('/user/profile', function () {
+    return Inertia::render('EditProfile');
+})->name('edit.profile');
 
 // Route::get('/bannerslider', function () {
 //     return Inertia::render('BannerSlider');
