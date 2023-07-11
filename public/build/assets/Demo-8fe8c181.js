e6e7758d606505e39bca5d8630b5d470186c5139
Muzhar Hussain <muzharhussain110@gmail.com>
2023-07-11 17:44:26 +0500

    build  Tue, Jul 11, 2023  5:44:26 PM

---
+++ public/build/assets/Demo-8fe8c181.js
@@ -0,0 +1,1 @@
+import{a as o}from"./app-0290a7fe.js";import{L as e}from"./Layout-8241d4a6.js";function t(){return o("h2",{children:"hello world from dmeo"})}t.layout=r=>o(e,{children:r});export{t as default};
---
+++ public/build/assets/bell-6fd89589.js
@@ -0,0 +1,1 @@
+const s="/build/assets/whatsapp-9dc030f1.png",a="/build/assets/bell-bd7d9f04.png";export{a as b,s as w};
