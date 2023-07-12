1ba9d4f695aba6a5c089f663ccce8b9667996dd9
Nahoor07 <nahoor98@gmail.com>
2023-07-12 17:13:50 +0500

    asd

--- resources/js/Components/Navbar/NabBar2.tsx
+++ resources/js/Components/Navbar/NabBar2.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import imagelogo1 from '@/Assets/headerlogo.png'
+import imagelogo1 from '@/Assets/nav2.png'
 import watsapImage from '@/Assets/whatsapp.png'
 import bellIcon from "@/Assets/bell.png"
 
@@ -17,7 +17,7 @@ const NavBar2 = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
-            <img className="w-52 h-9" src={imagelogo1} alt="Logo" />
+            <img className="w-52 h-9 object-contain" src={imagelogo1} alt="Logo" />
           </div>
           <div className="hidden md:block">
             <div className="flex items-center space-x-4">
