b2fd144d40302901831e98e6e15996948b332b60
wadood <wanikhan919@gmail.com>
2023-07-14 18:04:07 +0500

    build by wadood

--- public/build/assets/Demo-4537850f.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./app-4ec2a2cc.js";import{L as e}from"./Layout-49af0a27.js";function t(){return o("h2",{children:"hello world from dmeo"})}t.layout=r=>o(e,{children:r});export{t as default};
