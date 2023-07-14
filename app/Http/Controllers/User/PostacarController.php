1f71deb24d668cb101f64ad6d1805faf96623f37
Nahoor07 <nahoor98@gmail.com>
2023-07-14 12:42:58 +0500

    contact done

--- app/Http/Controllers/User/PostacarController.php
+++ app/Http/Controllers/User/PostacarController.php
@@ -32,11 +32,9 @@ class PostacarController extends Controller
      */
     public function store(Request $request)
     {
-        
         $request->validate([
             'title' => 'required',
             'brand_id' => 'required',
-         
             'condition' => 'required',
             'engineCapacity' => 'required',
             'mileage' => 'required',
@@ -53,7 +51,7 @@ class PostacarController extends Controller
             'images.*' => 'image',
         ],[
             'brand_id.required' =>'The brand field is required',
-      
+
         ]);
         $images = '';
         $arr=[];
@@ -89,7 +87,7 @@ class PostacarController extends Controller
         }else{
             return Inertia::location(route('user.dashboard', ['error' => 'Failed Car not added.']));
         }
-        
+
     }
 
     /**
@@ -103,17 +101,72 @@ class PostacarController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Postacar $postacar)
+    public function edit($slug)
     {
-        //
+        $brands=Brand::where('status',1)->get();
+        $car=Car::where('slug',$slug)->first();
+        $car->images=explode(",",$car->images);
+        return Inertia::render('User/EditCar',['brands'=>$brands,'car'=>$car]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Postacar $postacar)
+    public function update(Request $request,$id)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'brand_id' => 'required',
+            'condition' => 'required',
+            'engineCapacity' => 'required',
+            'mileage' => 'required',
+            'location' => 'required',
+            'price' => 'required',
+            'fuelType' => 'required',
+            'model' => 'required',
+            'transmission' => 'required',
+            'drive' => 'required',
+            'interiorColor' => 'required',
+            'exteriorColor' => 'required',
+            'description' => 'required',
+            'images.*' => 'image',
+        ],[
+            'brand_id.required' =>'The brand field is required',
+
+        ]);
+        $images = '';
+        $arr=[];
+        if ($request->hasFile('images')) {
+            foreach ($request->file('images') as $item) {
+                $var = date_create();
+                $time = date_format($var, 'YmdHis');
+                $imageName = $time . '-' . $item->getClientOriginalName();
+                $item->move(public_path('storage/images/cars'), $imageName);
+                array_push($arr, '/images/cars/' . $imageName);
+            }
+        }
+        $images = implode(",", $arr);
+        $model=Car::find($id);
+        $model->title=$request->title;
+        $model->brand_id= $request->brand_id;
+        $model->condition=$request->condition;
+        $model->engine_capacity=$request->engineCapacity;
+        $model->mileage=$request->mileage;
+        $model->location=$request->location;
+        $model->price=$request->price;
+        $model->drive=$request->drive;
+        $model->images=$images;
+        $model->fuel_Type=$request->fuelType;
+        $model->model=$request->model;
+        $model->transmission=$request->transmission;
+        $model->interior_color=$request->interiorColor;
+        $model->exterior_color=$request->exteriorColor;
+        $model->description=$request->description;
+        if($model->save()){
+            return Inertia::location(route('user.dashboard', ['success' => 'Car update successfully.']));
+        }else{
+            return Inertia::location(route('user.dashboard', ['error' => 'Failed to update car.']));
+        }
     }
 
     /**
--- resources/js/Pages/Front/ContactUsPage.tsx
+++ resources/js/Pages/Front/ContactUsPage.tsx
@@ -2,39 +2,87 @@
 import React from 'react'
 import Footer from '../Footer/Footer'
 import NavBar from '@/Components/Navbar/NavBar'
-
+import Bg from '@/Assets/bgcontact.png'
+// import backgroundedImage from '@/Assets/';
+import contactimg from "@/Assets/contactimg.png"
 const ContactUsPage = (auth: any) => {
-  return (
-    <>
- <NavBar auth={auth}/>
-      <section className="bg-white">
-        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contact Us</h2>
-          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
-          <form action="#" className="space-y-8">
-            <div>
-              <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900">Name</label>
-              <input type="text" id="name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="" required />
-            </div>
-            <div>
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
-              <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="" required />
-            </div>
-            <div>
-              <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900">Subject</label>
-              <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="" required />
-            </div>
-            <div className="sm:col-span-2">
-              <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-              <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder=""></textarea>
-            </div>
-            <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-emerald-500 sm:w-fit hover:bg-emerald-600 focus:ring-4 focus:outline-none focus:ring-primary-300 ">Send message</button>
-          </form>
-        </div>
-      </section>
-    <Footer/>
-    </>
-  )
+    return (
+        <>
+            <NavBar auth={auth} />
+            <section className="bg-white">
+                <div className="hero-section bg-cover bg-center h-96 flex justify-center items-center" style={{ backgroundImage: `url(${Bg})` }}>
+                    <div className="hero-content text-center">
+                        <h1 className="text-5xl text-emerald-500 font-bold ">Contact Us</h1>
+                        <p className="text-xl text-white">Home - Contact Us.</p>
+                    </div>
+                </div>
+                <div className="mx-auto max-w-screen-xl w-full h-full mt-12 p-12">
+                    <div className="border border-gray-500 rounded-xl shadow-md">
+                        <div className="grid grid-cols-1 md:grid-cols-2 ">
+                            <div className='mt-9 mx-4 my-4 align-center'>
+                                <h2 className="text-2xl text-center text-dark uppercase font-bold mt-12">Login</h2>
+                                <p className="text xl text-center text-dark mb-4">Got any issue ? No problem contact us now</p>
+                                <form>
+                                    <div className="flex flex-wrap gap-2 mb-3">
+                                        <div className="w-60">
+                                            <input
+                                                placeholder="First Name"
+                                                className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-50"
+                                            />
+                                        </div>
+                                        <div className="w-60">
+                                            <input
+                                                placeholder="Last Name"
+                                                className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-50"
+                                            />
+                                        </div>
+                                    </div>
+
+
+
+                                    <div className="mb-3 w-11/12">
+                                        <input
+                                            placeholder="Enter Email"
+                                            className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-50"
+                                        />
+                                    </div>
+                                    <div className="mb-3 w-11/12">
+                                        <input
+                                            placeholder="Phone Number"
+                                            className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-50"
+                                        />
+                                    </div>
+                                    <div className="mb-3 w-11/12">
+                                        <textarea
+                                            placeholder="Write your message"
+                                            className="w-full px-3 py-2 border border-gray-300 rounded-xl bg-gray-50"
+
+                                        ></textarea>
+
+                                    </div>
+
+
+
+                                    <div className="text-center">
+                                        <button
+                                            type="submit"
+                                            className="bg-emerald-500 hover:bg-emerald-600 text-white py-2 px-4 rounded-xl mt-2"
+                                        >
+                                            SEND NOW
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="hidden md:block rounded-r-xl overflow-hidden">
+                                <img src={contactimg} alt="" className="w-full h-full object-cover" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </>
+    )
 }
 
 export default ContactUsPage
