9adfaa56ececb30180670d04efad961bc877629b
wadood <wanikhan919@gmail.com>
2023-07-18 18:17:19 +0500

    build

--- public/build/assets/Demo-f9959163.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-ab08e8ee.js";import{L as t}from"./Layout-2b4f2449.js";import"./headerlogo-80979cd7.js";import"./index-e9a6f4f3.js";function m(){return o.jsx("h2",{children:"hello world from dmeo"})}m.layout=r=>o.jsx(t,{children:r});export{m as default};
---
+++ public/build/assets/transfer-57390ff6.js
@@ -0,0 +1,1 @@
+const s="/build/assets/Swapped-9d1678b2.png";export{s as T};
