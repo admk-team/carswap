a1c0ae23c23f375d4e18837eb7c8f2e4e56ea91c
Tariq Rahim <122568371+tariqrahimpk@users.noreply.github.com>
2023-07-20 17:39:19 +0500

    Changes in logo

--- resources/js/Components/HomePage/Featured.tsx
+++ resources/js/Components/HomePage/Featured.tsx
@@ -16,7 +16,7 @@ const Featured = () => {
             <div className="mx-auto max-w-screen-xl w-full h-full mt-8">
                 <h1 className='text-2xl md:text-3xl font-semibold text-center w-full '>Featured in</h1>
                     <div className='flex mt-6 justify-center items-center '>
-                    <div className='grid grid-cols-3 md:grid-cols-9 gap-10'>
+                    <div className='grid grid-cols-3 md:grid-cols-9 gap-10 ml-3 mr-3'>
                         <img src={Featured1} alt="" className='w-24 h-20 cursor-pointer'/>
                         <img src={Featured2} alt="" className='w-24 h-20 cursor-pointer'/>
                         <img src={Featured3} alt="" className='w-24 h-20 cursor-pointer'/>
