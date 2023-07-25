30c61fc595747f76e85db2fd6836ebbed24c1bf8
Nahoor07 <nahoor98@gmail.com>
2023-07-25 14:21:03 +0500

    image section done

--- resources/js/Pages/Front/CarDetail.tsx
+++ resources/js/Pages/Front/CarDetail.tsx
@@ -29,8 +29,15 @@ import ReviewForm from '@/Components/Forms/ReviewForm';
 import ReviewListing from '@/Components/Reviews/ReviewListing';
 import Conditon from "@/Assets/car-settings.png";
 import CarEngine from "@/Assets/car-engine.png";
-import ImageGallery from '@/Components/Slider/ImageGallery';
+// import ImageGallery from '@/Components/Slider/ImageGallery';
 import { Inertia } from '@inertiajs/inertia';
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+
+
+
+
+
 
 export default function CarDetail({ car, auth, similarCars, success, error, user_rating, my_cars }: any) {
     const [checkReview, setCheckReview] = useState(false);
@@ -47,6 +54,7 @@ export default function CarDetail({ car, auth, similarCars, success, error, user
     const [selectedMyCarImages, setSelectedMyCarImages] = useState([]);
     const [selectedMyCarPrice, setSelectedMyCarPrice] = useState(0);
     const [my_CarId, setMyCarId] = useState();
+    const [galleryImages,setGalleryImages] = useState([]);
 
     useEffect(() => {
         if (success) {
@@ -72,12 +80,12 @@ export default function CarDetail({ car, auth, similarCars, success, error, user
     });
 
     const carPrice = car?.price || 0;
-    const yourCarPrice = my_cars?my_cars[selectedCarIndex]?.price : 0;
+    const yourCarPrice = my_cars ? my_cars[selectedCarIndex]?.price : 0;
 
     const calculatePriceDifference = () => {
-      console.log("Calculating price difference...");
-      const difference = carPrice - ourCarPrice;
-      setPriceDifference(difference);
+        console.log("Calculating price difference...");
+        const difference = carPrice - ourCarPrice;
+        setPriceDifference(difference);
     };
 
 
@@ -131,7 +139,7 @@ export default function CarDetail({ car, auth, similarCars, success, error, user
 
     const paymentResponse = () => {
         if (paymentData !== null) {
-            Inertia.post(route('user.storePayment'), { paymentData,data });
+            Inertia.post(route('user.storePayment'), { paymentData, data });
         }
     }
     const handleFullImages = (ims: any) => {
@@ -144,7 +152,7 @@ export default function CarDetail({ car, auth, similarCars, success, error, user
         return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 
-    const handleSwapNowClick = (carId: any, myCarId:any, myCarTitle:any, myCarImage:any, myCarPrice:any) => {
+    const handleSwapNowClick = (carId: any, myCarId: any, myCarTitle: any, myCarImage: any, myCarPrice: any) => {
         setSelectedCarId(carId);
         setSelectedMyCarId(myCarId);
         setSelectedMyCarTitle(myCarTitle);
@@ -156,11 +164,11 @@ export default function CarDetail({ car, auth, similarCars, success, error, user
     };
 
     //For car swap
-    const { data, setData, post,} = useForm({
-        Inspection_date:'',
-        Inspection_Time:'',
-        car_id:car.id || null,
-        my_car_id:my_CarId || '',
+    const { data, setData, post, } = useForm({
+        Inspection_date: '',
+        Inspection_Time: '',
+        car_id: car.id || null,
+        my_car_id: my_CarId || '',
 
     });
     const handleSwapModalClose = () => {
@@ -170,14 +178,24 @@ export default function CarDetail({ car, auth, similarCars, success, error, user
 
 
     function handleSubmit() {
-        console.log("data",data);
+        console.log("data", data);
         post(route('user.swap.store'));
     }
-    console.log('images : ',images)
+
+    useEffect(()=>{
+        setGalleryImages(car.images.map((img:any)=>{
+            return {
+                original: '/storage' + img,
+                thumbnail: '/storage' + img,
+            };
+        }))
+    },[car])
+
+    useEffect(() => {
+        console.log(galleryImages);
+    }, [galleryImages])
     return (
-        <>
-            {images && (images.length > 0) && <ImageGallery images={images} setImages={setImages} />}
-            {(!images || (images?.length == 0)) &&
+
                 <div>
                     <Head title={car.title} />
                     <div>
@@ -220,14 +238,15 @@ export default function CarDetail({ car, auth, similarCars, success, error, user
                             <div className="grid grid-cols-12 gap-4 mt-7">
                                 <div className="col-span-12 md:col-span-6">
                                     <div className="flex flex-col">
-                                        <img src={'/storage/' + car.images[0]} className="w-full h-5/6 object-cover" alt="Cover Image" onClick={() => handleFullImages(car.images)} />
+                                    <ImageGallery items={galleryImages} showPlayButton={false}   />
+                                        {/* <img src={'/storage/' + car.images[0]} className="w-full h-5/6 object-cover" alt="Cover Image" onClick={() => handleFullImages(car.images)} />
                                         <div className="flex flex-wrap gap-2 mt-3">
                                             {
                                                 car.images.map((image: any, index: any) => (
                                                     <img key={index} src={'/storage/' + image} className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 xl:w-1/6" onClick={() => handleFullImages(car.images)} />
                                                 ))
                                             }
-                                        </div>
+                                        </div> */}
 
                                     </div>
                                 </div>
@@ -240,33 +259,33 @@ export default function CarDetail({ car, auth, similarCars, success, error, user
                                                         <h2 className="text-lg font-bold mb-4 text-center text-emerald-900">Swap Buy Calculator</h2>
                                                         <hr className='mb-4' />
                                                         <div className='flex flex-wrap p-3 bg-gray-100 justify-between rounded border shadow'>
-                                                            <p className='font-bold'>Car Price</p>
+                                                            <p className='font-bold'>Our Car Price</p>
                                                             <p>$ {formatNumberWithCommas(carPrice)}</p>
                                                         </div>
                                                         <div className='flex flex-wrap p-3 bg-gray-100 justify-between rounded border shadow mt-5'>
                                                             <p className='font-bold mt-1'>Your Car Price</p>
-                                                            <p>$ {formatNumberWithCommas(my_cars?my_cars[selectedCarIndex]?.price:0)}</p>
+                                                            <p>$ {formatNumberWithCommas(my_cars ? my_cars[selectedCarIndex]?.price : 0)}</p>
                                                         </div>
                                                         <div className='flex flex-wrap p-3 bg-gray-100 justify-between rounded border shadow mt-5'>
                                                             <p className='font-bold mt-1'>Price Difference</p>
                                                             <p>$ {formatNumberWithCommas(priceDifference)}</p>
                                                         </div>
                                                         {
-                                                            auth?.user?
+                                                            auth?.user ?
                                                                 <button
-                                                                className='bg-emerald-500 hover:bg-emerald-700 w-full text-white font-bold py-2 px-4 rounded mt-3'
-                                                                onClick={calculatePriceDifference}
+                                                                    className='bg-emerald-500 hover:bg-emerald-700 w-full text-white font-bold py-2 px-4 rounded mt-3'
+                                                                    onClick={calculatePriceDifference}
                                                                 >
                                                                     Calculate
                                                                 </button>
-                                                            :
-                                                            <button
-                                                                className='bg-emerald-500 hover:bg-emerald-700 w-full text-white font-bold py-2 px-4 rounded mt-3'
-                                                            >
-                                                                <Link href={route('user.login')}>
-                                                                    Calculate
-                                                                </Link>
-                                                            </button>
+                                                                :
+                                                                <button
+                                                                    className='bg-emerald-500 hover:bg-emerald-700 w-full text-white font-bold py-2 px-4 rounded mt-3'
+                                                                >
+                                                                    <Link href={route('user.login')}>
+                                                                        Calculate
+                                                                    </Link>
+                                                                </button>
                                                         }
                                                     </>
                                                     :
@@ -305,23 +324,23 @@ export default function CarDetail({ car, auth, similarCars, success, error, user
                                                                 <img src={Transfer} className="w-full h-20 object-contain"></img>
                                                             </div>
                                                             <div className="col-span-1 md:col-span-2 lg:col-span-1">
-                                                                <p className='text-gray-950 mt-2 text-2xl font-bold mb-2'>My Our</p>
-                                                                <Slider {...settings}>
-                                                                    {
-                                                                        auth?.user?
+                                                                <p className='text-gray-950 mt-2 text-2xl font-bold mb-2'>Our Car</p>
+                                                                {
+                                                                    auth?.user ?
                                                                         <>
-                                                                            {my_cars.map((my_car:any, index:any) => (
-                                                                                <div key={index} onClick={() => setSelectedCarIndex(index)}> {/* Add onClick handler */}
-                                                                                    <img src={'/storage' + my_car?.images[0]} className="w-full h-4/5 object-contain" alt={`Car ${index + 1}`} />
-                                                                                </div>
-                                                                            ))}
+                                                                            <Slider {...settings}>
+                                                                                {my_cars.map((my_car: any, index: any) => (
+                                                                                    <div key={index} onClick={() => setSelectedCarIndex(index)}> {/* Add onClick handler */}
+                                                                                        <img src={'/storage' + my_car?.images[0]} className="w-full h-4/5 object-contain" alt={`Car ${index + 1}`} />
+                                                                                    </div>
+                                                                                ))}
+                                                                            </Slider>
                                                                         </>
                                                                         :
                                                                         <div>
                                                                             <h4>Login First to view your cars to swap</h4>
                                                                         </div>
-                                                                    }
-                                                                </Slider>
+                                                                }
                                                             </div>
                                                         </div>
 
@@ -594,7 +613,7 @@ export default function CarDetail({ car, auth, similarCars, success, error, user
                                                 </div>
                                             ))}
                                             {showSwapModal && selectedCarId && (
-                                                <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-center items-center">
+                                                <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-center items-center z-10">
 
                                                     <div className="flex flex-col  w-full bg-white rounded p-4 max-w-md">
                                                         <h2 className="text-lg font-bold mb-4 text-center text-emerald-900">Book Inspection</h2>
@@ -636,18 +655,18 @@ export default function CarDetail({ car, auth, similarCars, success, error, user
                                                             value={data.Inspection_date} onChange={(e) => setData('Inspection_date', e.target.value)}
                                                         />
                                                         <div className="flex justify-end mt-4">
-                                                        <button   onClick={() => {
-                                                            handleFlutterPayment({
-                                                                callback: (response: any) => {
-                                                                    setPaymentData(response);
-                                                                    setTimeout(() => {
-                                                                        paymentResponse();
+                                                            <button onClick={() => {
+                                                                handleFlutterPayment({
+                                                                    callback: (response: any) => {
+                                                                        setPaymentData(response);
+                                                                        setTimeout(() => {
+                                                                            paymentResponse();
 
-                                                                    }, 3000);
-                                                                },
-                                                                onClose: () => { },
-                                                            });
-                                                        }}
+                                                                        }, 3000);
+                                                                    },
+                                                                    onClose: () => { },
+                                                                });
+                                                            }}
                                                                 className='bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded mr-2'
                                                             >
                                                                 Book Now
@@ -673,8 +692,7 @@ export default function CarDetail({ car, auth, similarCars, success, error, user
                     </div>
 
                 </div>
-            }
-        </>
+
 
 
     )
