920cdcc9c93ec0b5483dfe5c8a74de33a98b2c08
wadood <wanikhan919@gmail.com>
2023-07-26 17:38:02 +0500

    build by wadood

--- public/build/assets/InputLabel-3abbb838.js
+++
@@ -1,1 +0,0 @@
-import{a as m}from"./app-52f5e563.js";function o({value:t,className:e="",children:a,...r}){return m("label",{...r,className:"block font-medium text-sm text-gray-700 "+e,children:t||a})}export{o as I};
