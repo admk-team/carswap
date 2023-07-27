29b07dc2a5e8233b80ce5987da73e4d2a7a8fc75
Muzhar Hussain <muzharhussain110@gmail.com>
2023-07-27 15:13:16 +0500

    schema fix and revert email fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
-            $table->string('email', 250)->unique();
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone_no')->nullable();
