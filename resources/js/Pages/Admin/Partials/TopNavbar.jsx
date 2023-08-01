78bec0224d4e422af1cf8c6f263c0fde948c46ba
wadood <wanikhan919@gmail.com>
2023-08-01 16:51:30 +0500

    car done

--- resources/js/Pages/Admin/Partials/TopNavbar.jsx
+++ resources/js/Pages/Admin/Partials/TopNavbar.jsx
@@ -270,7 +270,6 @@ function TopNavbar({ onCollapseChange, auth }) {
                     >
                         <span className="mr-2 d-none d-lg-inline text-gray-600 small">
                             Douglas McGee
-                            
                         </span>
                         <img
                             className="img-profile rounded-circle"
