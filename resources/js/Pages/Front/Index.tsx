add102764ed72c0c9470db020727285e96489028
wadood <wanikhan919@gmail.com>
2023-08-16 14:09:39 +0500

    cars for swap title

--- resources/js/Pages/Front/Index.tsx
+++ resources/js/Pages/Front/Index.tsx
@@ -25,7 +25,7 @@ const Index: React.FC<IndexProps> = ({ brands,swap_cars,auth,sale_cars,categorie
       <NavBar auth={auth} />
       <TopBanner brands={brands} categories={categories}/>
       <div className="text-3xl text-center w-full mt-12">
-        <h1 className='text-gray-950 font-semibold'>Car For Swap</h1>
+        <h1 className='text-gray-950 font-semibold'>Cars for Swap</h1>
       </div>
       <PricingCard cars={swap_cars} />
       <div className="mx-auto flex justify-center max-w-screen-xl w-full h-full mt-12 ">
@@ -34,7 +34,7 @@ const Index: React.FC<IndexProps> = ({ brands,swap_cars,auth,sale_cars,categorie
       {/* <BannerTwo /> */}
       <Steps/>
       <div className="text-3xl text-center w-full mt-12">
-        <h1 className='text-gray-950 font-semibold'>Car For Sale</h1>
+        <h1 className='text-gray-950 font-semibold'>Cars for Sale</h1>
       </div>
       <PricingCard cars={sale_cars} />
       <div className="mx-auto flex justify-center max-w-screen-xl w-full h-full mt-12 ">
