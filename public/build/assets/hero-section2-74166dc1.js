d1a294b75590ff2dcb6ead143fea1802f6422b06
wadood <wanikhan919@gmail.com>
2023-09-15 14:00:20 +0500

    build Fri Sep 15 14:00:20 PST 2023

--- public/build/assets/hero-section2-74166dc1.js
+++
@@ -1,1 +0,0 @@
-const s="/build/assets/hero-section-bc0168e6.jpg",e="/build/assets/hero-section2-2203bd72.jpg";export{e as a,s as b};
---
+++ public/build/assets/hero-section2-c3648d56.js
@@ -0,0 +1,1 @@
+const e="/build/assets/hero-section-bc0168e6.jpg",s="/build/assets/hero-section2-239e9e0a.jpg";export{s as a,e as b};
