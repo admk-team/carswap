ea9420ec8415cb0b43268b34a46d65aceeaf32a5
Nahoor07 <nahoor98@gmail.com>
2023-09-15 17:08:50 +0500

    view more removed

--- resources/js/Pages/Front/Index.tsx
+++ resources/js/Pages/Front/Index.tsx
@@ -45,9 +45,9 @@ const Index: React.FC<IndexProps> = ({ brands,swap_cars,auth,sale_cars,categorie
         <h1 className='text-gray-950 font-semibold'>Cars for Sale</h1>
       </div> */}
       {/* <PricingCard cars={sale_cars} /> */}
-      <div className="mx-auto flex justify-center max-w-screen-xl w-full h-full mt-12 ">
+      {/* <div className="mx-auto flex justify-center max-w-screen-xl w-full h-full mt-12 ">
         <Link href={route('ViewAllCars','sale')} className="text-white bg-green-500 hover:bg-green-600 focus:outline-none font-medium  text-sm px-6 py-2.5 text-center dark:bg-amber-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-32">View More</Link>
-      </div>
+      </div> */}
       {/* <Reviews /> */}
       {/* <AfterStep/> */}
       {/* <WhyUs/> */}
