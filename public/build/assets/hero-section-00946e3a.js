9bf8643f24b319a2ad9db08e7ee90cfca2a8237b
Nahoor07 <nahoor98@gmail.com>
2023-09-15 16:54:51 +0500

    build

---
+++ public/build/assets/hero-section-00946e3a.js
@@ -0,0 +1,1 @@
+const e="/build/assets/hero-section-bc0168e6.jpg";export{e as b};
--- public/build/assets/hero-section2-c3648d56.js
+++
@@ -1,1 +0,0 @@
-const e="/build/assets/hero-section-bc0168e6.jpg",s="/build/assets/hero-section2-239e9e0a.jpg";export{s as a,e as b};
